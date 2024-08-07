73a4c86281d9e10a389d94d46b6c50260424373e
Hamidreza <hamidreza.mugen@gmail.com>
2024-08-07 13:35:11 +0330

    indirect

---
+++ Mechanic_Indirect.py
@@ -0,0 +1,15 @@
+class MechanicIndirect(Mechanic):
+    def __init__(self):
+        super().__init__()
+        self.__base_price = 250000
+        self.__price_per_hour = 100000
+        self.__speed = 60
+
+    def get_base(self):
+        return self.__base_price
+    def get_hour(self):
+        return self.__price_per_hour
+    def get_speed(self):
+        return self.__speed
+    def calculate_total_price(self,service_time):
+        return self.get_base() + self.get_hour() * service_time
